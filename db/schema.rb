62e681a8bcdb33ed23c0d3c9948a7f21f15f408d
Koome <judekoome360@gmail.com>
2023-06-28 08:35:46 +0300

    Completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_182338) do
+ActiveRecord::Schema.define(version: 2023_06_28_051347) do
+
+  create_table "article_tags", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "directors", force: :cascade do |t|
     t.string "name"
@@ -38,6 +48,11 @@ ActiveRecord::Schema.define(version: 2021_05_24_182338) do
     t.string "date"
     t.string "url"
     t.integer "movie_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
